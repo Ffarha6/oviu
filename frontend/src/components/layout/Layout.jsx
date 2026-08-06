5d61a3860caee95bb674abe34a4d74be21fd64fe
Ffarha6 <yasserf263@gmail.com>
2026-08-06 17:12:07 +0300

    Fix hero layout spacing

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -32,7 +32,7 @@ function Layout({ children }) {
       */}
       <main
         className={`overflow-x-hidden w-full ${hideMobileBottomBar ? "" : "pb-20 lg:pb-0"}`}
-        style={{ paddingTop: "132px" }}
+        style={{ paddingTop: "112px" }}
       >
         {children}
       </main>
