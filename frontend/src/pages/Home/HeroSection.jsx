3745e19fb09e71de7972eeed3c8daaebf20b77ed
Ffarha6 <yasserf263@gmail.com>
2026-08-10 03:48:17 +0300

    add hero image slider

--- frontend/src/pages/Home/HeroSection.jsx
+++ frontend/src/pages/Home/HeroSection.jsx
@@ -140,6 +140,7 @@ useEffect(() => {
     <div
       className="
         hidden sm:flex
+        
         md:px-0
         pb-0
         md:pb-2
@@ -155,7 +156,6 @@ useEffect(() => {
           relative
           overflow-hidden
           aspect-[16/9]
-          lg:aspect-[21/8]
           bg-[#F8F4F1]
         "
       >
